a1ad7f82c12f6f7f1addbe43eff8f3b4f52a0d36
James Moulang <jamesmoulang123@gmail.com>
2016-12-10 23:22:24 +0000

    starting, npm init

--- Game.js
+++ Game.js
@@ -1,6 +1,74 @@
+import _ from 'underscore';
+
 class Game {
-	constructor(parent) {
-		console.log("starting a new game with parent " + parent);
+	constructor(parentID, states) {
+		this.parentID = parentID;
+		this.gamePadding = 64;
+		this.gameWidth = 1024;
+		this.gameHeight = 1024;
+
+		this.states = states;
+		this.canvases = [];
+		this.canvasIndex = 0;
+		window.onresize = this.resizeCanvases.bind(this);
+		document.onmouseover = this.onmousemove.bind(this);
+		document.onmousemove = this.onmousemove.bind(this);
+		document.onclick = this.onmouseclick.bind(this);
+		document.onmousedown = this.onmousedown.bind(this);
+		document.onmouseup = this.onmouseup.bind(this);
+	}
+
+	addState(state) {
+
+	}
+
+	start(key) {
+
+	}
+
+	loop() {
+
+	}
+
+	update() {
+
+	}
+
+	render() {
+
+	}
+
+	createCanvas(key) {
+		var canvas = new Canvas(this.parent, this.canvasIndex, 'canvas_' + key, this.gamePadding, this.gameWidth, this.gameHeight);
+		this.canvasIndex++;
+		this.canvases.push(canvas);
+		return canvas;
+	}
+
+	resizeCanvases() {
+		_.each(this.canvases, function(c) {
+			c.resize();
+		});
+		this.drawBackground();
+		this.renderGrid();
+		this.redrawStones();
+	}
+
+	//Mouse input
+	onmousemove() {
+
+	}
+	onmousemove() {
+
+	}
+	onmouseclick() {
+
+	}
+	onmousedown() {
+
+	}
+	onmouseup() {
+
 	}
 }
 
